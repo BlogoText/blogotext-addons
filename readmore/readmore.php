d2691063038c5d59e0d91d6d4c3497380022555d
Tiger-222 <contact@tiger-222.fr>
2016-11-22 17:15:14 +0100

    - [readmore] Few corrections to comply with PHP Mess Detector - [readmore] Add cool style (taken from couleur-science.eu)

--- readmore/readmore.php
+++ readmore/readmore.php
@@ -6,59 +6,81 @@
 
 $GLOBALS['addons'][] = array(
     'tag' => 'readmore',
-    'name' => 'Read more',
-    'desc' => 'List 3 "read-also like" thumbnails below each post.',
+    'name' => array(
+        'en' => 'Read more',
+        'fr' => 'Autres articles',
+    ),
+    'desc' => array(
+        'en' => 'List 3 "read-also like" thumbnails below each post.',
+        'fr' => 'Afficher des image d\'autres articles.',
+    ),
     'version' => '1.0.0',
+    'css' => 'style.css',
+
+    'config' => array(
+        'nb_posts' => array(
+            'type' => 'int',
+            'label' => array(
+                'en' => 'Number of posts to list',
+                'fr' => 'Nombre d\'articles à lister'
+            ),
+            'value' => 4,
+            'value_min' => 1,
+            'value_max' => 8,
+        ),
+    ),
 );
 
 function addon_readmore()
 {
-    $nb_art = 3;
-    // lists IDs
+    $conf = addon_get_conf('readmore');
+    $nbPosts = $conf['nb_posts']['value'];
+
+    // Find all posts
+    $sql = '
+        SELECT ID
+          FROM articles
+         WHERE bt_statut = 1
+               AND bt_date <= '.date('YmdHis');
     try {
-        $result = $GLOBALS['db_handle']->query("SELECT ID FROM articles WHERE bt_statut=1 AND bt_date <= ".date('YmdHis'))->fetchAll(PDO::FETCH_ASSOC);
+        $result = $GLOBALS['db_handle']->query($sql)->fetchAll(PDO::FETCH_ASSOC);
     } catch (Exception $e) {
-        die('Error rand addon_readmore(): '.$e->getMessage());
+        return ((bool)DISPLAY_PHP_ERRORS) ? 'Error addon_readmore(): '.$e->getMessage() : '';
     }
 
-    // clean array
-    foreach ($result as $i => $art) {
-        $result[$i] = $art['ID'];
+    // Clean array
+    foreach ($result as $i => $post) {
+        $result[$i] = (int)$post['ID'];
     }
-    // randomize array
+
+    // Select N entries
     shuffle($result);
+    $posts = array_slice($result, 0, $nbPosts);
 
-    // select nth entries (PHP does take care about "nb_arts > count($result)")
-    $art = array_slice($result, 0, $nb_art);
-
-    // get articles
+    // Get posts
+    $sql = '
+        SELECT bt_title, bt_id, bt_content
+          FROM articles
+         WHERE ID IN ('.implode(',', $posts).')';
     try {
-        $array_qmark = str_pad('', count($art)*3-2, "?, ");
-        $query = "SELECT bt_title, bt_id, bt_content FROM articles WHERE bt_statut=1 AND bt_date <= ".date('YmdHis')." AND ID IN (".$array_qmark.")";
-        $req = $GLOBALS['db_handle']->prepare($query);
-        $req->execute($art);
-        $articles = $req->fetchAll(PDO::FETCH_ASSOC);
+        $posts = $GLOBALS['db_handle']->query($sql)->fetchAll(PDO::FETCH_ASSOC);
     } catch (Exception $e) {
-        die('Error fetch content addon_readmore(): '.$e->getMessage());
+        return ((bool)DISPLAY_PHP_ERRORS) ? 'Error fetch content addon_readmore(): '.$e->getMessage() : '';
     }
 
-    foreach ($articles as $i => $article) {
-        // extract image from $article[bt_content]
-        preg_match('<img *.* src=(["|\']?)(([^\1 ])*)(\1).*>', $article['bt_content'], $matches);
-        $articles[$i]['bt_img'] = '';
-        if (!empty($matches)) {
-            $articles[$i]['bt_img'] = chemin_thb_img_test($matches[2]);
+    // Generates the list
+    $html = '<ul id="readmore">'."\n";
+    foreach ($posts as $i => $post) {
+        // Extract the image from $post['bt_content']
+        preg_match('<img *.* src=(["|\']?)(([^\1 ])*)(\1).*>', $post['bt_content'], $matches);
+        $img = '';
+        if ($matches) {
+            $img = $matches[2];  // chemin_thb_img_test($matches[2])
         }
-        unset($articles[$i]['bt_content']);
-        // generates link
-        $dec_id = decode_id($article['bt_id']);
-        $articles[$i]['bt_link'] = $GLOBALS['racine'].'?d='.implode('/', $dec_id).'-'.titre_url($article['bt_title']);
-    }
-
-    // generates the UL/LI list.
-    $html = '<ul>'."\n";
-    foreach ($articles as $art) {
-        $html .= "\t".'<li style="background-image: url('.$art['bt_img'].');"><a href="'.$art['bt_link'].'">'.$art['bt_title'].'</a></li>'."\n";
+        // Generates the link
+        $decId = decode_id($post['bt_id']);
+        $link = $GLOBALS['racine'].'?d='.implode('/', $decId).'-'.titre_url($post['bt_title']);
+        $html .= "\t".'<li style="background-image: url('.$img.');"><a href="'.$link.'">'.$post['bt_title'].'</a></li>'."\n";
     }
     $html .= '</ul>'."\n";
 
