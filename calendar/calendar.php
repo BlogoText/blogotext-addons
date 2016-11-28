e28964a5b56e0bad43f88d5beb4439e17ed440ae
RemRem <remrem@users.noreply.github.com>
2016-11-28 14:14:30 +0100

    PSR, my bad...

--- calendar/calendar.php
+++ calendar/calendar.php
@@ -8,7 +8,7 @@
  * Changelog
  *
  * 0.1.0 2016-11-28 RemRem
- *  - upd addon to be BT#160 compliant 
+ *  - upd addon to be BT#160 compliant
  *  - fix #12
  *  - upd current version to 0.X (dev version)
  */
