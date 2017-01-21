ee7e92d0c0aa54d9bdb63ca986f4d4a39c35d245
Thuban <thuban@yeuxdelibad.net>
2017-01-21 19:47:56 +0100

    write latex code with katex instead of mathjax

---
+++ latex/katex-config.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+    renderMathInElement(
+        document.body,
+        {
+            delimiters: [
+                {left: "$$", right: "$$", display: true},
+                {left: "$", right: "$", display: false},
+            ]
+        }
+    );
+};
