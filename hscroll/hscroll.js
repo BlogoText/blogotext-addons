cebda1170a2140ea41962312257123d32c78fbce
Thuban <thuban@yeuxdelibad.net>
2017-01-22 08:29:27 +0100

    horizontal scrollbar indicator

---
+++ hscroll/hscroll.js
@@ -0,0 +1,13 @@
+// stolen from http://www.tiger-222.fr/?d=2016/10/18/14/00/25-scrollbar-horizontale
+function scroll_bar() {
+    'use strict';
+    var t = document.querySelector('#scroll-bar'),
+        a = document.body.clientHeight,
+        n = window.innerHeight,
+        g = window.pageYOffset,
+        o = g / (a - n) * 100;
+
+    t.style.width = o + '%';
+}
+window.addEventListener('load', scroll_bar);
+window.addEventListener('scroll', scroll_bar);
---
+++ hscroll/hscroll.php
@@ -0,0 +1,31 @@
+<?php
+
+$declaration = array(
+    // the tag of your addon (required)
+    'tag' => 'hscroll',
+
+    // the name, showed in admin/addon (required)
+    'name' => array(
+        'en' => 'Horizontal scrollprogress',
+        'fr' => 'Indicateur de lecture',
+    ),
+
+    // the desc, showed in admin/addon (required)
+    'desc' => array(
+        'en' => 'Horizontal reading progressbar',
+        'fr' => 'Indicateur horizontal de lecture',
+    ),
+
+    // the version, showed in admin/addon (required)
+    'version' => '0.1.0',
+    'compliancy' => '3.7',
+    'css' => 'hscroll.css',
+    'js' => 'hscroll.js',
+    'url' => 'http://www.tiger-222.fr/?d=2016/10/18/14/00/25-scrollbar-horizontale',
+);
+
+function a_hscroll()
+{
+    $html = '<div id="scroll-bar"><div id="scroll-bar-inner"></div></div>';
+    return $html;
+}
