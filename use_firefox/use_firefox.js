b8f7762a2d4ed62fb990624bd2fc6537348aca2e
Thuban <thuban@yeuxdelibad.net>
2018-01-30 13:19:35 +0100

    showrss to dev (#48)
    * Use firefox (#33)
    
    * fix small mistake, forgot ,
    
    * Show alert if firefox unused
    
    * add some free browsers
    
    * travis
    
    * changement de logique
    
    * minify
    
    * use id instead
    
    * Show rss (#32)
    
    * fix small mistake, forgot ,
    
    * simple rss item show
    
    * change default feed

---
+++ use_firefox/use_firefox.js
@@ -0,0 +1,29 @@
+"use strict";
+
+var modal = document.getElementById('use_ffx_modal');
+
+// Get the <span> element that closes the modal
+var span = document.getElementById("use_ffx_close");
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+if ((navigator.userAgent.toLowerCase().indexOf('chrome') > -1) ||
+    (navigator.userAgent.toLowerCase().indexOf('opera') > -1) ||
+    (navigator.userAgent.toLowerCase().indexOf('vivaldi') > -1) ||
+    (navigator.userAgent.toLowerCase().indexOf('safari') > -1) ||
+    (navigator.userAgent.toLowerCase().indexOf('msie') > -1) ||
+    (navigator.userAgent.toLowerCase().indexOf('samsung') > -1) ||
+    (navigator.userAgent.toLowerCase().indexOf('nokia') > -1) ||
+    (navigator.userAgent.toLowerCase().indexOf('chromium') > -1)) {
+        modal.style.display = "block";
+}
